544c6eb527380366a5aca6c208c4cf05bc1007b9
rbdavid <russell.davidson@colostate.edu>
2016-06-24 15:09:25 -0600

    x axis is now plotted as time w/ units of ns

--- dihedral_plotting.py
+++ dihedral_plotting.py
@@ -31,6 +31,12 @@ chi1_data = np.loadtxt(chi1_file)
 my_cmap = plt.cm.get_cmap('jet')
 my_cmap.set_under('w')
 
+nSteps = len(phi_data)
+
+time = np.zeros(nSteps)
+for i in range(nSteps):
+	time[i] = i*0.002		# units of time in ns; each frame is separated by 0.002 ns 
+
 for i in range(nRes):
 # IF ONLY ONE RESIDUE IN RES_LIST FILE
 #	counts, xedges, yedges, image = plt.hist2d(phi_data[:], psi_data[:], bins=100, cmap=my_cmap,vmin = 0.1)
@@ -55,11 +61,12 @@ for i in range(nRes):
 # IF ONLY ONE RESIDUE IN RES_LIST FILE
 #	plt.plot(omega_data[:],'k.')
 # IF MULTIPLE RESIDUES IN RES_LIST FILE
-	plt.plot(phi_data[:,i],'k.')
+	plt.plot(time[:],phi_data[:,i],'k.')
 	plt.grid(b=True, which='major', axis='both', color='#808080', linestyle='--')
-	plt.xlabel(r'Time (timesteps)', size=12)
+	plt.xlabel(r'Time (ns)', size=12)
 	plt.ylabel(r'Phi Dihedral', size=12)
 	plt.ylim((-180,180))
+	plt.xlim((0,1500))
 	plt.yticks(np.arange(-180,181,90))
 	plt.savefig('%03d.%s.phi_dihedral.png' %(res_list.res[i],system))
 	plt.close()
@@ -68,11 +75,12 @@ for i in range(nRes):
 # IF ONLY ONE RESIDUE IN RES_LIST FILE
 #	plt.plot(omega_data[:],'k.')
 # IF MULTIPLE RESIDUES IN RES_LIST FILE
-	plt.plot(psi_data[:,i],'k.')
+	plt.plot(time[:],psi_data[:,i],'k.')
 	plt.grid(b=True, which='major', axis='both', color='#808080', linestyle='--')
-	plt.xlabel(r'Time (timesteps)', size=12)
+	plt.xlabel(r'Time (ns)', size=12)
 	plt.ylabel(r'Psi Dihedral', size=12)
 	plt.ylim((-180,180))
+	plt.xlim((0,1500))
 	plt.yticks(np.arange(-180,181,90))
 	plt.savefig('%03d.%s.psi_dihedral.png' %(res_list.res[i],system))
 	plt.close()
@@ -80,11 +88,12 @@ for i in range(nRes):
 # IF ONLY ONE RESIDUE IN RES_LIST FILE
 #	plt.plot(omega_data[:],'k.')
 # IF MULTIPLE RESIDUES IN RES_LIST FILE
-	plt.plot(omega_data[:,i],'k.')
+	plt.plot(time[:],omega_data[:,i],'k.')
 	plt.grid(b=True, which='major', axis='both', color='#808080', linestyle='--')
-	plt.xlabel(r'Time (timesteps)', size=12)
+	plt.xlabel(r'Time (ns)', size=12)
 	plt.ylabel(r'Omega Dihedral', size=12)
 	plt.ylim((-180,180))
+	plt.xlim((0,1500))
 	plt.yticks(np.arange(-180,181,90))
 	plt.savefig('%03d.%s.omega_dihedral.png' %(res_list.res[i],system))
 	plt.close()
@@ -92,11 +101,12 @@ for i in range(nRes):
 # IF ONLY ONE RESIDUE IN RES_LIST FILE
 #	plt.plot(chi1_data[:],'k.')
 # IF MULTIPLE RESIDUES IN RES_LIST FILE
-	plt.plot(chi1_data[:,i],'k.')
+	plt.plot(time[:],chi1_data[:,i],'k.')
 	plt.grid(b=True, which='major', axis='both', color='#808080', linestyle='--')
-	plt.xlabel(r'Time (timesteps)', size=12)
+	plt.xlabel(r'Time (ns)', size=12)
 	plt.ylabel(r'Chi1 Dihedral', size=12)
 	plt.ylim((-180,180))
+	plt.xlim((0,1500))
 	plt.yticks(np.arange(-180,181,90))
 	plt.savefig('%03d.%s.chi1_dihedral.png' %(res_list.res[i],system))
 	plt.close()
